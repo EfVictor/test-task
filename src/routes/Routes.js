0c9c5a93dc1fde3aa10f2153daed09ff44a1d704
EfVictor <efimov840@gmail.com>
2025-12-30 14:59:56 +0300

    Update

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const сontrollers = require("../controllers/Controller");             // Здесь описаны все контроллеры
+const controllers = require("../controllers/Controller");             // Здесь описаны все контроллеры
 const validateWriteOff = require("../middlewares/validateWriteOff");  // Мидлварка для валидации входных данных
 
 const router = express.Router();
 
-router.post("/writeOff", validateWriteOff, сontrollers.writeOff)   // Маршрут для списания/пополнения баланса пользователя
-router.get("/Balance/:user_id", сontrollers.getBalance)         // Маршрут для получения баланса пользователя
-router.get("/", сontrollers.emptyQuery)                            // Маршрут для пустого запроса
+router.post("/writeOff", validateWriteOff, controllers.writeOff)   // Маршрут для списания/пополнения баланса пользователя
+router.get("/Balance/:user_id", controllers.getBalance)            // Маршрут для получения баланса пользователя
+router.get("/", controllers.emptyQuery)                            // Маршрут для пустого запроса
 
 module.exports = router;
